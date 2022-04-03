94d95f680cd2e153c2afa439027939b6cab0b19b
quedacoder <quedacoder@gmail.com>
2022-04-03 16:26:29 -0500

    Initial project setup

---
+++ src/main/java/com/quedacoder/todorestfulwebservices/TodoRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.quedacoder.todorestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quedacoder/todorestfulwebservices/TodoRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quedacoder.todorestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
